bc32297b369d82863abb3a20ec036bd79c41762c
itlizePD <irisdu1996@gmail.com>
2020-12-08 02:17:41 -0500

    finish all the requirement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Clock';
+export class AppComponent implements OnInit, OnDestroy {
+  title = "Clock";
+  time = new Date();
+  hour:any;
+  minute:any;
+  second:any;
+  ampm:any;
+  timer:any;
+  isShow = true;
+  isChange = false;
+
+  ngOnInit() {
+    this.timer = setInterval(() => {
+      this.time = new Date();
+      this.hour = this.time.getHours();
+      this.minute = this.time.getMinutes();
+      this.second = this.time.getSeconds();
+      this.ampm = this.hour >= 12 ? 'PM' : 'AM';
+      this.hour = this.hour % 12;
+      this.hour = this.hour ? this.hour : 12;
+      this.minute = this.minute < 10 ? '0'+this.minute : this.minute;
+      this.second = this.second < 10 ? '0'+this.second : this.second;
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+
+  changeTime(event:any){
+    clearInterval(this.timer);
+    this.isShow = false;
+    this.isChange = true;
+  }
+
+  changeHour(event:any) {
+    this.hour = event.target.value;
+  }
+    changeMinute(event:any) {
+    this.minute = event.target.value;
+  }
+
+    changeSecond(event:any) {
+    this.second = event.target.value;
+  }
+
+  resume(event:any){
+    this.isShow = true;
+    this.isChange = false;
+    this.timer = setInterval(() => {
+      let newtime = parseInt(this.second)+1;
+      this.time = new Date(0,0,0,this.hour,this.minute,newtime);
+      this.hour = this.time.getHours();
+      this.minute = this.time.getMinutes();
+      this.second = this.time.getSeconds();
+      this.ampm = this.hour >= 12 ? 'PM' : 'AM';
+      this.hour = this.hour % 12;
+      this.hour = this.hour ? this.hour : 12;
+      this.minute = this.minute < 10 ? '0'+this.minute : this.minute;
+      this.second = this.second < 10 ? '0'+this.second : this.second;
+    }, 1000);
+  }
 }
